4bc3192dc46080b4d9efb5b79942ba001832e5e0
Mhasan Fuad <76463319+mhasanfuadd22@users.noreply.github.com>
2021-03-18 23:31:49 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+// untuk menambahkan tinggal copy yang ada terus paste di bawahnya, 
+// ganti kode negara sesuai countrycode.org
+
+$linkOffer = array( 
+    "ALL" => "https://vozkn.shewantyou.com/c/da57dc555e50572d?s1=74105&s2=1143981&s3=xnxx&click_id=LEAD_TEROS&j1=1&j3=1", // Yang ini jangan dihapus, sebagai default
+    "IN" => "https://www.youtube.com/",
+    "ID" => "https://www.google.com/",
+    "AR" => "https://www.google.com/",
+    "MA" => "https://www.google.com/",
+);
