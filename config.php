3382e9414e9d809dc014061c6842754100a76421
Mhasan Fuad <76463319+mhasanfuadd22@users.noreply.github.com>
2021-03-18 23:34:10 +0700

    Update config.php

--- config.php
+++ config.php
@@ -4,9 +4,7 @@
 // ganti kode negara sesuai countrycode.org
 
 $linkOffer = array( 
-    "ALL" => "https://vozkn.shewantyou.com/c/da57dc555e50572d?s1=74105&s2=1143981&s3=xnxx&click_id=LEAD_TEROS&j1=1&j3=1", // Yang ini jangan dihapus, sebagai default
-    "IN" => "https://www.youtube.com/",
-    "ID" => "https://www.google.com/",
-    "AR" => "https://www.google.com/",
-    "MA" => "https://www.google.com/",
+    "ALL" => "https://vozkn.hotgirlforyou.com/c/da57dc555e50572d?s1=74105&s2=1207576&s3=porn&click_id=MESERI&j1=1&j3=1", // Yang ini jangan dihapus, sebagai default
+    "RO" => "https://vozkn.imitrack1.com/c/5af57103f16229bc?s1=74105&s2=115094&s3=porn&s5=MAP&click_id=MAP20",
+    "ID" => "https://www.instagram.com/",
 );
